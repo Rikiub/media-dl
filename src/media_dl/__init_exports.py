095e614b2e30f22d598f90efebc360fa6083f05f
Rikiub <rikiub.dev@gmail.com>
2026-01-05 20:36:23 -0400

    chore: Fix typing imports

--- src/media_dl/__init_exports.py
+++ src/media_dl/__init_exports.py
@@ -1,4 +1,4 @@
-from media_dl.downloader.stream import StreamDownloader
-from media_dl.models.format import AudioFormat, VideoFormat
-from media_dl.models.playlist import Playlist
-from media_dl.models.stream import Stream
+from media_dl.downloader.stream import StreamDownloader  # noqa: F401
+from media_dl.models.format import AudioFormat, VideoFormat  # noqa: F401
+from media_dl.models.playlist import Playlist  # noqa: F401
+from media_dl.models.stream import Stream  # noqa: F401
