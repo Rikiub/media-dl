3c4f82b01bbc1d34498964a062893b1bc6b613e3
Rikiub <rikiub.dev@gmail.com>
2026-02-04 17:38:54 -0400

    feat: add progress callbacks to public API

--- packages/lib/src/remora/__init__imports.py
+++ packages/lib/src/remora/__init__imports.py
@@ -1,6 +1,16 @@
-from remora.downloader.main import MediaDownloader  # noqa: F401
-from remora.exceptions import DownloadError, ExtractError  # noqa: F401
-from remora.extractor import MediaExtractor  # noqa: F401
-from remora.models.content.list import LazyPlaylist, Playlist, Search  # noqa: F401
-from remora.models.content.media import LazyMedia, Media  # noqa: F401
-from remora.models.format.types import AudioFormat, VideoFormat  # noqa: F401
+# ruff: noqa: F401
+
+from remora.downloader.main import MediaDownloader
+from remora.exceptions import (
+    MediaError,
+    DownloadError,
+    ExtractError,
+    ProcessingError,
+    OutputTemplateError,
+)
+from remora.extractor import MediaExtractor
+from remora.models.content.list import LazyPlaylist, Playlist, Search
+from remora.models.content.media import LazyMedia, Media
+from remora.models.format.types import AudioFormat, VideoFormat
+from remora.models.progress.list import PlaylistDownloadState
+from remora.models.progress.media import MediaDownloadState
